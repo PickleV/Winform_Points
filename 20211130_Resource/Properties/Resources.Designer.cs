fb51506576be91ffec64002d7a02fce927a31d13
Vincent <vincent.cc.lu@gmail.com>
2021-11-30 12:41:05 -0500

    Added sensor

--- 20211130_Resource/Properties/Resources.Designer.cs
+++ 20211130_Resource/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace _20211130_Resource.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap sensor {
+            get {
+                object obj = ResourceManager.GetObject("sensor", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
